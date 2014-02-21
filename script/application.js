20e3298a17cb6465bc6941bf93b7b718ba0f731f
manokdlp3 <manokdlp3@gmail.com>
2014-02-21 12:22:56 -0600

    test files

--- script/application.js
+++ script/application.js
@@ -1,1 +1,16 @@
-/* Insert some javascript code here */
+$(document).ready(function() {
+	toolkit.init();
+});
+
+var toolkit = {
+	init: function() {
+		$(".button").on("click", this.changeMessage);
+		$(".button").on("mouseenter", function() { $(".button").toggleClass("highlight");});
+		$(".button").on("mouseleave", function() { $(".button").toggleClass("highlight");});
+	},
+	changeMessage: function(e) {
+		e.preventDefault();
+		//$("h1").toggleClass("highlight");
+		$(".blurb").fadeToggle();
+	}
+};
